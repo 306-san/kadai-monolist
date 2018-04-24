32a3b9126e14fab2421a0fded3c2255650e5ef2b
306-san <oh.taku528+git@gmail.com>
2018-04-24 10:41:13 +0000

    have

--- app/controllers/ownerships_controller.rb
+++ app/controllers/ownerships_controller.rb
@@ -12,8 +12,10 @@ class OwnershipsController < ApplicationController
     # Want 関係として保存
     if params[:type] == 'Want'
       current_user.want(@item)
-      flash[:success] = '商品を Want しました。'
+    elsif params[:type] == 'Have'
+      current_user.have(@item)
     end
+    flash[:success] = "商品を #{params[:type]} しました。" unless params[:type].nil?
 
     redirect_back(fallback_location: root_path)
   end
@@ -23,8 +25,10 @@ class OwnershipsController < ApplicationController
 
     if params[:type] == 'Want'
       current_user.unwant(@item) 
-      flash[:success] = '商品の Want を解除しました。'
+    elsif params[:type] == 'Have'
+      current_user.unhave(@item)
     end
+    flash[:success] = "商品の #{params[:type]} を解除しました。" unless params[:type].nil?
 
     redirect_back(fallback_location: root_path)
   end
